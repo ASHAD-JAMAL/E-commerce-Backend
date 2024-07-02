4cf3b006f50f4567c22b807320453ba8b3d7df00
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-07-02 22:43:29 +0530

    middleware added

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const httpStatusCode = require("../constants/httpStatusCode");
+
+async function getToken(user){
+    const token = await jwt.sign({user},process.env.JWT_SECRET,{expiresIn:"1d"});
+    return token;
+}
+
+async function verifyToken(req,res,next){
+    const token = req.body.headers.Authorization;
+
+    if(!token){
+        return res.status(httpStatusCode.UNAUTHORIZED).json({
+            success:false,
+            message:"Unauthorized: Token not provided"
+        });
+    }
+    try{
+        //split the authorization header by space and directly use the token
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        req.user= decoded.user;
+        next();
+    }catch(error){
+        console.error("Error Verifying Token:",error);
+        return res.status(httpStatusCode.UNAUTHORIZED).json({
+            success:false,
+            message:"Unauthorized: Invalid Token"
+        });
+    }
+}
+module.exports={
+    getToken,
+    verifyToken
+}
