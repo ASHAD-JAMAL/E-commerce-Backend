7a2d89486c8f13553d6a6b9eda2e5a86fd1e5649
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-07-12 23:54:42 +0530

    model added

---
+++ models/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstname:{
+            type:String,
+            required:true,
+        },
+        lastname:{
+            type:String,
+            required:true,
+        },
+        username:{
+            type:String,
+            required:true,
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+        },
+        phone:{
+            type:String,
+            required:true,
+        },
+        profileImage:{
+            type:String,
+            required:true,
+        },
+        password:{
+            type:String,
+            required:true,
+        },
+        city:{
+            type:String,
+            required:true,
+        },
+        state:{
+            type:String,
+            required:true,
+        },
+        zip:{
+            type:String,
+            required:true,
+        },
+        role:{
+            type:String,
+            required:true,
+        }
+    },
+    {timestamps:true}
+);
+module.exports = mongoose.model("User",UserSchema);
